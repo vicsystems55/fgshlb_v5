d942d22c2f796d276283f2f7e7904ed1b3186f56
vicSystesms <46149752+vicsystems55@users.noreply.github.com>
2020-12-15 21:18:40 +0100

    new codebase

--- resources/views/layouts/fullLayoutMaster.blade.php
+++ resources/views/layouts/fullLayoutMaster.blade.php
@@ -48,6 +48,18 @@ $configData = Helper::applClasses();
     {{-- include default scripts --}}
     @include('panels/scripts')
 
+    <script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5fd910eadf060f156a8d671d/1epjuk63c';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+
 </body>
 
 </html>
--- resources/views/layouts/verticalLayoutMaster.blade.php
+++ resources/views/layouts/verticalLayoutMaster.blade.php
@@ -61,17 +61,19 @@ $configData = Helper::applClasses();
   @include('panels/scripts')
 
   <!--Start of Tawk.to Script-->
+<!--Start of Tawk.to Script-->
 <script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/5dd76f2a43be710e1d1e911f/default';
+s1.src='https://embed.tawk.to/5fd910eadf060f156a8d671d/1epjuk63c';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 </script>
+<!--End of Tawk.to Script-->
 <!--End of Tawk.to Script-->
 
 </body>
