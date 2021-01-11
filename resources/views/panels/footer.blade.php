c069a0dd516d3fee6f241c3e88b14620b4bcbc6b
vicSystesms <46149752+vicsystems55@users.noreply.github.com>
2021-01-11 12:28:50 +0100

    new codebase

--- resources/views/panels/footer.blade.php
+++ resources/views/panels/footer.blade.php
@@ -7,7 +7,7 @@
         class="footer {{ $configData['footerType'] }}  {{($configData['footerType']=== 'footer-hidden') ? 'd-none':''}} footer-light">
         @endif
         <p class="clearfix blue-grey lighten-2 mb-0"><span
-                class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2020<a
+                class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2021<a
                     class="text-bold-800 grey darken-2" href="https://fgshlb.gov.ng"
                     target="_blank">Federal Government Staff Houseing Loan Board</a>Powered by Knowledge Factory ICT Solutions</span>
             <button class="btn btn-primary btn-icon scroll-top" type="button"><i
