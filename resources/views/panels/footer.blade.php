526ff217069396c289b4d33a3ffb2a341335a64c
vicSystesms <46149752+vicsystems55@users.noreply.github.com>
2021-01-11 12:25:23 +0100

    new codebase

--- resources/views/panels/footer.blade.php
+++ resources/views/panels/footer.blade.php
@@ -8,7 +8,7 @@
         @endif
         <p class="clearfix blue-grey lighten-2 mb-0"><span
                 class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2020<a
-                    class="text-bold-800 grey darken-2" href="https://1.envato.market/pixinvent_portfolio"
+                    class="text-bold-800 grey darken-2" href="https://fgshlb.gov.ng"
                     target="_blank">Federal Government Staff Houseing Loan Board</a>Powered by Knowledge Factory ICT Solutions</span>
             <button class="btn btn-primary btn-icon scroll-top" type="button"><i
                     class="feather icon-arrow-up"></i></button>
